c59e168582ff9a85e86e3078b203c7d12239de4c
Arjun Somvanshi <arjunsomvanshi@gmail.com>
2021-12-06 02:39:23 +0530

    Implementation of signup and login funcitonsjh:w

--- src/main.py
+++ src/main.py
@@ -1,17 +1,95 @@
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager, SlideTransition
+from kivy.properties import ListProperty, StringProperty, DictProperty, BooleanProperty, NumericProperty
+from kivy.utils import rgba as RGBA
+from kivy.core.window import Window
+from security import *
+import os
 
-kv = Builder.load_string('''
-BoxLayout:
-    Button:
-        text: "hello"
-        ''')
+app = None
+Window.clearcolor = RGBA('#292B2F')
+
+class Screen_Manager(ScreenManager):
+    pass
+class SignUp(Screen):
+    def signup_and_verify(self):
+        if self.ids.password.text == self.ids.cpassword.text:
+            if len(self.ids.password.text) > 8:
+                if len(self.ids.username.text) > 4:
+                    global app
+                    username_hash = blake(self.ids.username.text)                
+                    password_hash = blake(self.ids.password.text)                
+                    credentials = username_hash + password_hash
+                    credentials = credentials.encode("utf-8")
+                    os.mkdir("../UserData/")
+                    app.write_file("Credentials.bin", credentials)
+                    self.manager.transition = SlideTransition(direction="right")
+                    self.manager.current = "login"
+                else:
+                    self.ids.message.text = "Username must be greater than 4 characters."
+            else:
+                self.ids.message.text = "Password should be greater than 8 characters."
+        else:
+            self.ids.message.text = "The passwords do not match, try again."
+
+class Login(Screen):
+    def auth(self):
+        username_hash = blake(self.ids.username.text)                
+        password_hash = blake(self.ids.password.text)                
+        credentials = username_hash + password_hash
+        stored_hash = app.read_file("Credentials.bin").decode("utf-8")
+        if stored_hash == credentials:
+            self.ids.message.text = "Success"
+        else:
+            self.ids.message.text = "Login Failed"
+        
+    def signup(self):
+        global app
+        if app.checkUser():
+            self.ids.message.text = "User Exists Already."
+        else:
+            self.manager.transition = SlideTransition(direction="left")
+            self.manager.current = "signup"
+class ViewRecords(Screen):
+    pass
 
 class healthNodeApp(App):
-    #This is the instance of the app object
+    fonts = DictProperty({"main": "Fonts/Montserrat-SemiBold.ttf", "regular": "Fonts/Montserrat-Regular.ttf"})
+    theme = DictProperty({"primary_dark": RGBA("#292B2F"), "primary": RGBA("#2F3136"), 
+                          "secondary_dark": RGBA("#36393F"), "secondary": RGBA("#40444B"),
+                          "font1": RGBA("#697279"), "font2": RGBA("#ffffff")
+                        })
+    home = StringProperty('../UserData/')
+
+    def read_file(self, file):
+        with open(self.home+file, 'rb') as f:
+            return f.read()
+
+    def write_file(self, file, content):
+        with open(self.home+file, 'wb') as f:
+            f.write(content)
+    
+    def write_json_file(self, file, content):
+        with open(self.home+file, 'w') as f:
+            json.dump(content, f, indent=2)
+
+    def read_json_file(self, file, content):
+        with open(self.home+file, 'w') as f:
+            return json.load(f)
+
+    def checkUser(self):
+        try:
+            if os.path.isfile("../UserData/Credentials.bin"):
+                return True
+            return False
+        except:
+            return False
+
+    def on_start(self):
+        Window.clearcolor = self.theme["primary_dark"]
     def build(self):
-        #This functions runs when the app is lauched
-        return kv
+        global app
+        app = self
 
 if __name__ == "__main__":
     healthNodeApp().run()
---
+++ src/security.py
@@ -0,0 +1,162 @@
+# Module for security protocols of reminiscor
+'''
+Reminiscor is a free offline password manager.
+Copyright (C) 2020 Arjun Somvanshi & Manvendra Somvanshi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+'''
+Following is the way to define argon two parameters:
+    * set the number of iteration to 2
+    * choose how many devices you will use the database on, and note the ram of the device which has the least memory, let that be m
+    * the memory parameter should be the least of m/2 and 1
+    * Find out the number of logical processors of each of your devices. Set the parallelism parameter to the minimum of these numbers. On Windows 10, 
+      the number of logical processors can be found in the Task Manager (right-click onto the taskbar → 'Task Manager') on the 'Performance' tab page.
+'''
+
+import hashlib
+from Crypto.Random import get_random_bytes
+import json
+import argon2
+from base64 import b64encode, b64decode
+import sys
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+def blake (data): # send strings or bytes
+    if type(data) == str:
+        data = data.encode('utf-8')
+    hash1 = hashlib.blake2b(data)
+    data = None # unassigning the value of data
+    return hash1.hexdigest()
+
+def keyfile_encryption(key1):
+    keyfile_data = get_random_bytes(16)
+    keyfile_cipher = ECB_encrypt(keyfile_data, key1[:16])
+    with open(HomeDir('KeyFile.dat', 'UserData'), 'wb') as f:
+        f.write(keyfile_cipher)
+    return keyfile_data
+
+def keyfile_decrypt(key1, keyfile_dir):
+    with open(keyfile_dir, 'rb') as f:
+        keyfile_cipher = f.read() 
+    keyfile_data = ECB_decrypt(keyfile_cipher, key1[:16])
+    return keyfile_data
+
+def master_key(key1, first = False, re_encrypting = False, key2 = None): 
+    '''
+        Description
+        ***********
+            Used to derive the master key from argon 2 using keyfile and master password
+        Paramerters:
+        ************
+            key1 = The master
+    '''
+    # read the argon2 parameters before kdf
+    param = read_remfile('app_config.json')
+    #print("these are the configs: ", param)
+    # gotta get random salt only when database is re-encrypted
+    if first or re_encrypting: # re_encrypting should be true when reencrypting the database
+        salt = get_random_bytes(16)
+        write_remfile('master_salt.bin', salt)
+    else:
+        salt = read_remfile('master_salt.bin') # reading the defined salt
+    
+    if key2 is not None: # incase there is no keyfile involved
+        #print('password hash: ', key1,'\nkeyfile: ', key2)
+        #print(key1, key2)
+        composite_key = key1[:16].encode('utf-16') + key2[:16]
+    
+    else:
+        #print(key1)
+        composite_key = key1[:32].encode('utf-16')
+    #unassigning the sensitive data
+    key1 = None
+    key2 = None
+    #print(composite_key) uncomment to test the compostie key value
+    #print(salt)
+    masterkey = argon2.low_level.hash_secret_raw(secret = composite_key, 
+                                                 salt = salt, 
+                                                 time_cost = param['argon2_settings']['time_cost'], 
+                                                 memory_cost=param['argon2_settings']['memory_cost'], 
+                                                 parallelism= param['argon2_settings']['parallelism'], 
+                                                 hash_len = 32, 
+                                                 type=argon2.Type.D, 
+                                                 version=19)# deriving the AES KEY for data base
+    #unassigning the sensitive data
+    composite_key = None
+    return masterkey
+
+def master_key_store(master_key): # This stores the master key's hash value
+    key_hash = blake(master_key)
+    write_remfile('master_key_hash.bin', key_hash.encode('utf-8'))
+
+def auth_hash(master_key):
+    key_hash = blake(master_key) 
+    stored_hash = read_remfile('master_key_hash.bin').decode('utf-8')
+    if key_hash == stored_hash:
+        return True
+    return False
+
+def ECB_encrypt(plaintext, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    ct = cipher.encrypt(plaintext)
+    return ct
+
+def ECB_decrypt(cipher_text, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    pt = cipher.decrypt(cipher_text)
+    return pt
+
+def AES_Encrypt(key, data): # The data should be in bytes here
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+    ct = b64encode(ct_bytes).decode('utf-8')
+    iv = b64encode(ECB_encrypt(cipher.iv, key)).decode('utf-8')
+    encrypted_data = {'iv': iv, 'ct': ct}
+    return encrypted_data
+
+    
+def AES_Decrypt(key, encrypted_data): # enrypted_data is a dictionary which contains the encrypted form of the iv and the encrypted data
+    iv = ECB_decrypt(b64decode(encrypted_data['iv']), key) # decoding from base64 and then getting the iv back by decrypting it
+    ct = b64decode(encrypted_data['ct'])
+    cipher = AES.new(key, AES.MODE_CBC, iv = iv )
+    pt_bytes = unpad(cipher.decrypt(ct), AES.block_size)
+    return pt_bytes
+
+'''this function is for testing the key generation'''
+def composite_key_test(first):
+    k1 = blake('arjun2000')
+    #k2= blake('arjun')
+    return master_key(key1 = k1,key2 = None, first = first)
+
+def test_encryption(): # to test the encryption
+    database = {'entries':{'username': 'arjun somvanshi', 'password': 'arjun2000123455667'}}
+    database_as_bytes = json.dumps(database).encode('utf-8')
+    print(database_as_bytes)
+    key = composite_key_test(False)
+    e_data = AES_Encrypt(key, database_as_bytes)
+    print(e_data['iv'],'\n',e_data['ct'])
+    d = AES_Decrypt(key, e_data)
+    print(json.loads(d))
+    '''tester = get_random_bytes(32)
+    print(key, '\n', tester, sys.getsizeof(key), sys.getsizeof(tester))'''
+    
+'''write_remfile(write=True)
+write_AppConfig()
+key = composite_key_test(True)
+key1 = get_random_bytes(32)
+master_key_store(key)
+print(auth_hash(key1))'''
